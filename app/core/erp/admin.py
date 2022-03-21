ecbdaceab958df786d73bfdaba96a8385f2c9e5a
Yamil Orozco <yamiloro@gmail.com>
2022-03-20 23:15:41 -0400

    beat adding views and templates

--- app/core/erp/admin.py
+++ app/core/erp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.erp.models import *
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Sale)
+admin.site.register(DetSale)
--- app/core/erp/views.py
+++ app/core/erp/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from core.erp.models import Category, Product
 # Create your views here.
+
+
+def myfirstview(request):
+    data = {
+        'name':'William',
+        'categories': Category.objects.all()
+    }
+    return render(request, 'index.html', data)
+
+def mysecondview(request):
+    data = {
+        'name':'William',
+        'products': Product.objects.all(),
+        'categories': Category.objects.all()
+    }
+    return render(request, 'second.html', data)
